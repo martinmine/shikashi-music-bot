9157deadfbab0852259374cb4d224901a4ba2123
martinmine <martin_mine@hotmail.com>
2017-08-12 18:08:00 +0200

    Skip command

---
+++ ShikashiBot/Commands/Skip.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using ShikashiBot.Services;
+using System.Threading.Tasks;
+
+namespace ShikashiBot.Commands
+{
+    class Skip : ModuleBase
+    {
+        public SongService SongService { get; set; }
+
+        [Command("skip"), Summary("Skips current song")]
+        public async Task SkipSong()
+        {
+            SongService.Next();
+            await ReplyAsync("Skipped song");
+        }
+    }
+}
