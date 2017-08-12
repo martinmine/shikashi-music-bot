fcbb9b2079353bcf01e89b5de4ae14b69a7ccc0f
martinmine <martin_mine@hotmail.com>
2017-08-12 18:05:03 +0200

    Clear command

---
+++ ShikashiBot/Commands/Clear.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using ShikashiBot.Services;
+using System.Threading.Tasks;
+
+namespace ShikashiBot.Commands
+{
+    class Clear : ModuleBase
+    {
+        public SongService SongService { get; set; }
+
+        [Command("clear"), Summary("Clears all songs in queue")]
+        public async Task ClearQueue()
+        {
+            SongService.Clear();
+            await ReplyAsync("Queue cleared");
+        }
+    }
+}
