b41f8915c86f88974d34e78ac920c426984b3fc6
martinmine <martin_mine@hotmail.com>
2017-08-13 23:30:33 +0200

    Fixed typo

--- ShikashiBot/Deploy.ps1
+++ ShikashiBot/Deploy.ps1
@@ -1,4 +1,4 @@
 dotnet publish --framework netcoreapp1.1 -c release
-docker build -t martinmine/discord-music-bot .
-docker push martinmine/discord-music-bot
+docker build -t martinmine/shikashi-music-bot .
+docker push martinmine/shikashi-music-bot
 bash push-prod.sh
